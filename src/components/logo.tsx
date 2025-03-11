410bc1270c0518bf9d51a21d24074f7f67a4b98a
Silver <silvygermail@gmail.com>
2025-03-11 14:31:56 -0500

    change demo centering to better fit all screens

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,7 +7,7 @@ type LogoProps = {
 export const Logo = (props : LogoProps) => {
   return (
     <>
-        <img style={{width: 128, height: "auto", ...props}} className="logo" src="assets/images/logo.png" alt="Atlas Chicken Games, Ltd."/>
+        <img style={{width: "auto", height: 128, aspectRatio: 0.8076, ...props}} className="logo" src="assets/images/logo.png" alt="Atlas Chicken Games, Ltd."/>
     </>
   );
 };
