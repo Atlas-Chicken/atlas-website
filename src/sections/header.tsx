e80e72aa32f0bf25c4beb460612ea07f74eccfda
Silver <silvygermail@gmail.com>
2025-03-12 14:15:45 -0500

    in case viewport is null

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -6,7 +6,9 @@ import { motion, useScroll, useTransform } from "framer-motion";
 export const Header = () => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({ target: ref });
-  const y = useTransform(scrollYProgress, [0,1], [ 0, window.visualViewport?.height * 0.5 ]);
+  const viewport = window.visualViewport;
+  const viewportY = viewport ? viewport.height : 0;
+  const y = useTransform(scrollYProgress, [0,1], [ 0, viewportY * 0.5 ]);
 
   return (
 	<>
