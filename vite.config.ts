d7263493618087d72a9cbf350381958a67febba3
Silver <silvygermail@gmail.com>
2025-03-23 07:31:03 -0500

    remove terminal, errors with current vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Terminal from 'vite-plugin-terminal'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), Terminal({console:"terminal"})],
+  plugins: [react()],
 })
